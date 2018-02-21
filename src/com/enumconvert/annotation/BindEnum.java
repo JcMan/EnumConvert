943af10422784c5cfd50d9f29fc6cc5dd88a96ba
JcMan <pjcmail@163.com>
2018-02-21 22:53:55 +0800

    add Inherited

--- src/com/enumconvert/annotation/BindEnum.java
+++ src/com/enumconvert/annotation/BindEnum.java
@@ -1,11 +1,9 @@
 package com.enumconvert.annotation;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 @Target({ElementType.FIELD})
+@Inherited
 @Retention(RetentionPolicy.RUNTIME)
 public @interface BindEnum {
     Class enumClass();
